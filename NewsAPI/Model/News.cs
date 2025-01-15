d8a9ba7751eaecd0b7c8ffeae361ca7ce60960b3
abpprkonsalting <abpprkonsalting@gmail.com>
2025-01-15 09:52:46 -0500

    feat(NewsAPI/Model): add News model

---
+++ NewsAPI/Model/News.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsAPI.Model
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Body { get; set; }
+        public int ImageUrl { get; set; }
+
+    }
+}
